c81781ab3d3a5090c35f57b816f100ac83ea23c4
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-10-09 09:18:28 -0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+var pg = require('pg');
+
+var express = require('express');
+var app = express();
+var knex = require('./db/knex');
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
