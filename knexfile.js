2ad31f569952b1660ad28eb03c38ec52da651d59
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-10-09 11:33:52 -0400

    Fucking around with knex bullshit

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,35 @@
 // Update with your config settings.
 
-module.exports = {
+// module.exports = {
+//
+//   development: {
+//     client: 'pg',
+//     connection: {
+//       database: 'ncmp_2'
+//     }
+//   },
+//
+//   production : {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL
+//   }
+//
+// };
 
+var dotenv = require('dotenv');
+
+dotenv.load();
+
+module.exports = {
   development: {
     client: 'pg',
     connection: {
       database: 'ncmp_2'
     }
   },
-
   production : {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+      database: process.env.DATABASE_URL
   }
-
 };
