1cc8fa21f929a181ae61ba5844ab89b3439afb6a
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-11-25 05:35:25 -0500

    push to update

--- controllers/eventGroups.js
+++ controllers/eventGroups.js
@@ -38,20 +38,39 @@ exports.getEventGroups = function(req, res, next) {
 exports.addEventGroup = function(req, res, next) {
 
   console.log(req.body);
+
+  var name = req.body.name
+
+
   knex('event_groups')
   .insert({
-    name:req.body[0].name,
-    description:req.body[0].description,
-    color:req.body[0].color
+    name:req.body.info.name,
+    description:req.body.info.description,
+    color:req.body.info.color,
+    picture_ids: '['+req.body.image+']'
   })
   .then(function() {
-    return knex('event_groups')
-    .select('id')
-    .where({name:req.body[0].name})
-    .then(function(data) {
-      res.send(data);
-    })
+    res.send('success');
+  })
+  .catch(function(err) {
+    console.log(err);
   })
 
+
+}
+
+exports.getEventGroup = function(req, res, next) {
+
+  console.log(req.body)
+
+  knex('event_groups')
+  .select('id')
+  .where({name:req.body.info.name})
+  .then(function(data) {
+    res.send(data);
+  })
+  .catch(function(err) {
+    console.log(err)
+  })
 
 }
