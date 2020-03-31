c7d2157f985b82946683c56a246c9032485e8a11
Sam Cate <samcate@Sams-MacBook-Pro.local>
2020-03-31 03:58:20 -0400

    changed knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,36 +10,36 @@ module.exports = {
     }
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'samcate',
+      // user:     'username',
+      password: 'ncmpTk88'
+    },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'samcate',
+      // user:     'username',
+      password: 'ncmpTk88'
+    },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  }
 
 };
